2a9b4535f6bafe4016dfac16bb8737c77733866f
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-12-09 18:05:20 +0530

    msm: camera_v2: Import X00T Changes from S429
    * Based on S429 Kernel Source
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -259,9 +259,9 @@ enum msm_camera_i2c_operation {
 	MSM_CAM_WRITE = 0,
 	MSM_CAM_POLL,
 	MSM_CAM_READ,
-	MSM_CAM_READ_PAGE,
-	MSM_CAM_WRITE_DELAYUSEC,
-	MSM_CAM_READ_CONTINUOUS,
+	/*Huaqin add third supply front camera hi846 tsp by lizihao at 2018/04/10 start*/
+	MSM_CAM_SINGLE_LOOP_READ,
+	/*Huaqin add third supply front camera hi846 tsp by lizihao at 2018/04/10 end*/
 };
 
 struct msm_sensor_i2c_sync_params {
