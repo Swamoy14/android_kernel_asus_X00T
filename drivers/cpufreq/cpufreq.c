0df477d8aca7351bbc1d96e4c90f2c456bbf645b
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-01-24 13:53:08 +0600

    thermal: core: Refactor Xiaomi changes slightly
    - Do not use CPUFREQ_INCOMPATIBLE while using CPUFREQ_THERMAL.
    - Reformat Xiaomi changes using clang-format.
    - Rework on function names.
    - Use DEVICE_ATTR_[RW|RO] instead of using DEVICE_ATTR with explicit permission bits.
    - Make board sensor read-only.
    - Print missing cpu limits node creation failure message.
    
    Inspired from: https://github.com/EmanuelCN/kernel_xiaomi_sm8250/commit/fc12c2ba04648e96f179807edc19025a968d98d6
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2266,13 +2266,14 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_ADJUST, new_policy);
 
+#ifdef CONFIG_MACH_LONGCHEER
+	/* the adjusted frequency should not exceed thermal limit*/
+	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
+			CPUFREQ_THERMAL, new_policy);
+#else
 	/* adjust if necessary - hardware incompatibility */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_INCOMPATIBLE, new_policy);
-
-#ifdef CONFIG_MACH_LONGCHEER
-	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
-			CPUFREQ_THERMAL, new_policy);
 #endif
 
 	/*
